f16761bb706f52f176d8034027fad0b088504af3
Kunal Kundu <kunal99kundu@gmail.com>
2021-07-09 19:33:38 +0530

    Set16a completed

--- Set16a.hs
+++ Set16a.hs
@@ -18,7 +18,7 @@ import Data.List
 --  +++ OK, passed 1 test.
 
 isSorted :: (Show a, Ord a) => [a] -> Property
-isSorted = todo
+isSorted xs = sort xs === xs
 
 ------------------------------------------------------------------------------
 -- Ex 2: In this and the following exercises, we'll build a suite of
@@ -50,7 +50,7 @@ isSorted = todo
 --  +++ OK, passed 1 test.
 
 sumIsLength :: Show a => [a] -> [(a,Int)] -> Property
-sumIsLength input output = todo
+sumIsLength input output = length input === (sum $ map snd output)
 
 -- This is a function that passes the sumIsLength test but is wrong
 freq1 :: Eq a => [a] -> [(a,Int)]
@@ -79,7 +79,12 @@ freq1 (x:y:xs) = [(x,1),(y,length xs + 1)]
 --  +++ OK, passed 100 tests.
 
 inputInOutput :: (Show a, Eq a) => [a] -> [(a,Int)] -> Property
-inputInOutput input output = todo
+inputInOutput input output = forAll (elements input) (propIsIn output)
+
+propIsIn :: (Eq a) => [(a,Int)] -> a -> Property
+propIsIn xs r = (check $ lookup r xs) === True
+  where check x = case x of Nothing  -> False
+                            (Just x) -> True
 
 -- This function passes both the sumIsLength and inputInOutput tests
 freq2 :: Eq a => [a] -> [(a,Int)]
@@ -110,7 +115,10 @@ freq2 xs = map (\x -> (x,1)) xs
 --  +++ OK, passed 100 tests.
 
 outputInInput :: (Show a, Eq a) => [a] -> [(a,Int)] -> Property
-outputInInput input output = todo
+outputInInput input output = forAll (elements output) (propN input)
+
+propN :: (Eq a) => [a] -> (a, Int) -> Property
+propN xs t = (length $ filter (== fst t) xs) === snd t
 
 -- This function passes the outputInInput test but not the others
 freq3 :: Eq a => [a] -> [(a,Int)]
@@ -139,7 +147,9 @@ freq3 (x:xs) = [(x,1 + length (filter (==x) xs))]
 --  +++ OK, passed 100 tests.
 
 frequenciesProp :: ([Char] -> [(Char,Int)]) -> NonEmptyList Char -> Property
-frequenciesProp freq input = todo
+frequenciesProp freq (NonEmpty input) =
+  conjoin $ (map (\p -> p input output) [sumIsLength, inputInOutput, outputInInput])
+    where output = freq input
 
 frequencies :: Eq a => [a] -> [(a,Int)]
 frequencies [] = []
@@ -170,8 +180,11 @@ frequencies (x:ys) = (x, length xs) : frequencies others
 --  [2,4,10]
 
 genList :: Gen [Int]
-genList = todo
-
+genList = do
+  let a = elements [0..10]
+  n <- choose (3,5)
+  l <- vectorOf n a
+  return $ sort l
 ------------------------------------------------------------------------------
 -- Ex 7: Here are the datatypes Arg and Expression from Set 15. Write
 -- Arbitrary instances for Expression and Arg such that:
@@ -208,7 +221,13 @@ data Expression = Plus Arg Arg | Minus Arg Arg
   deriving (Show, Eq)
 
 instance Arbitrary Arg where
-  arbitrary = todo
+  arbitrary = oneof [vars, nums]
+    where vars = elements $ map (Variable) ['a','b','c','x','y','z']
+          nums = elements $ map (Number) [0..10]
 
 instance Arbitrary Expression where
-  arbitrary = todo
+  arbitrary = do
+    a <- arbitrary :: Gen Arg
+    b <- arbitrary :: Gen Arg
+    c <- elements [Plus, Minus]
+    return (c a b)
